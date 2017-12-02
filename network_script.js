0bf7e61c3b4df657b122426f08fadf436b6c4d79
kvtoraman <kvtoraman@gmail.com>
2017-12-03 02:01:19 +0900

    Increase distance of the circles - network.html

--- network_script.js
+++ network_script.js
@@ -185,9 +185,16 @@ function color(index){
 }
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(100).strength(1))
+    //.force("link", d3.forceLink())
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
+    //
+    // var force = d3.layout.force()
+    //     .gravity(.05)
+    //     .charge(-240)
+    //     .linkDistance(50)
+    //     .size([width, height]);
 
 together();
 
